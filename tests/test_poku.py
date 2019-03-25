81f41df7c7782e403a17e18fade1713ca88d7aca
sharktamer <donohoe.shane@gmail.com>
2019-03-25 19:49:29 +0000

    comment test functions

--- tests/test_poku.py
+++ tests/test_poku.py
@@ -12,15 +12,18 @@ import configargparse
 
 
 def test_parse_consumer():
+    """ Test that a consumer argument is handled and received """
     args = poku.parse_args(['--consumer', 'abc'])
     assert args.consumer == 'abc'
 
 
 def test_no_consumer():
+    """ Test that missing out the consumer argument causes a system exit """
     with pytest.raises(SystemExit):
         args = poku.parse_args([])
 
 
 def test_request_response():
+    """ Test pocket endpoint """
     response = requests.get('https://getpocket.com')
     assert response.ok

4f8ebbad96b0b5ce7f629378004f1e419f73ce2c
sharktamer <donohoe.shane@gmail.com>
2019-03-25 19:23:30 +0000

    add consumer arg parsing

--- poku/poku.py
+++ poku/poku.py
@@ -1,3 +1,13 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+import configargparse
+
+
+def parse_args(args):
+    conf_files = ['/etc/poku/*.cfg', '~/.config/poku/*.cfg']
+    parser = configargparse.ArgParser(default_config_files=conf_files)
+    parser.add('--consumer', '-p', required=True)
+
+    return parser.parse_args(args)
--- tests/test_poku.py
+++ tests/test_poku.py
@@ -8,6 +8,17 @@ import pytest
 
 from poku import poku
 import requests
+import configargparse
+
+
+def test_parse_consumer():
+    args = poku.parse_args(['--consumer', 'abc'])
+    assert args.consumer == 'abc'
+
+
+def test_no_consumer():
+    with pytest.raises(SystemExit):
+        args = poku.parse_args([])
 
 
 def test_request_response():

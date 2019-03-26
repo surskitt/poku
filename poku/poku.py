411cc72559917dcb1c740bb8ef84b8b970311013
sharktamer <donohoe.shane@gmail.com>
2019-03-26 23:59:16 +0000

    more info

--- poku/poku.py
+++ poku/poku.py
@@ -28,6 +28,7 @@ def main():
     if not args.access:
         request_token = poku.pocket.get_request_token(consumer_key)
         auth_url = poku.pocket.generate_auth_url(request_token)
+        print('Opening {} in browser'.format(auth_url))
         webbrowser.open(auth_url)
         input('Press Enter here once auth request is approved')
         access_token = poku.pocket.get_access_token(consumer_key,
